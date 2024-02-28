58e0f67a64fefbf36cd446660d301f6a2d80d61b
otkhwae <otkhwae@gmail.com>
2024-02-28 14:38:29 +0200

    setup dynamodb variables

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,29 @@ import boto3
 import json
 import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodbTableName = 'product-inventory'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamodbTableName)
+
+getMethod = 'GET'
+postMethod = 'POST'
+patchMethod = 'PATCH'
+deleteMethod = 'DELETE'
+
+healthPath = '/health'
+productPath = '/product'
+productsPath = '/products'
+
+
+
+
+
+
+
+
 
 
 
