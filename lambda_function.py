e569d6bec89ccd44eb89b97e85d50007e04f727d
otkhwae <otkhwae@gmail.com>
2024-02-28 14:53:27 +0200

    status code

--- lambda_function.py
+++ lambda_function.py
@@ -18,6 +18,35 @@ healthPath = '/health'
 productPath = '/product'
 productsPath = '/products'
 
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            'Content-Type' : 'application/json',
+            'Access-Control-Allow-Origin' : '*'
+        }
+    }
+    
+    if body is not None:
+        response['body'] = json.dumps(body)
+
+def lambda_handler (event, context):
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    endpointPath = event['path']
+    
+    if httpMethod == getMethod and endpointPath == healthPath:
+        response = buildResponse(200)
+
+
+
+
+
+
+
+
+
+
 
 
 
